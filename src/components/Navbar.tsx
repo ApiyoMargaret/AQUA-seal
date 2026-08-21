84f62576497b9948dff73e7c7430845df4020067
kc-clintone <kayseeclintone@gmail.com>
2026-08-21 16:42:04 +0300

    add src/components/Navbar.tsx

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,18 @@
+import React, { useEffect, useState } from 'react';
+import {
+  Anchor,
+  Phone,
+  MessageSquare,
+  PlusCircle,
+  Wifi,
+  WifiOff,
+  RefreshCw,
+  Layers,
+  ShoppingBag,
+  ShieldCheck,
+  Award,
+  Thermometer,
+  CreditCard,
+} from 'lucide-react';
+import { LANDING_SITES } from '../types/aqua-seal';
+import { offlineQueue } from '../lib/offline-queue';
